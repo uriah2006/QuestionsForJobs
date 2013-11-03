2e65132486a26b1b789124e22d9f2ead35fb65cd
uriah2006 <uriah2006@gmail.com>
2013-11-02 23:10:56 -0400

    working. palindrome working. added commits

--- src/palindrome/palindrome.java
+++ src/palindrome/palindrome.java
@@ -1,10 +1,41 @@
 package palindrome;
 
+import java.util.Scanner;
+
 public class palindrome {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Declarations
+		int i;
+		boolean palindrome=true;
+		String word;
+		Scanner input = new Scanner(System.in);
+		// Declarations end
+		
+		
+		//gets the word that need to be evaluated
+		System.out.println("enter the word you would like to be evaluated:");
+		word = input.next();
+		input.close();
+		
+		//loop that check the letters from out side in
+		for (i = 0; i < word.length() / 2; i++) {
+			
+			
+		// looks out the out side letters and checks them if they don't match
+		// don't check any more 
+			if (!(word.charAt(i)==word.charAt((word.length() - 1) - i))){
+				System.out.println(word+" is not a palindrome");
+				palindrome=false;
+				break;
+			}
+		}
+		
+		
+		// if word is a palindrome
+		if (palindrome){
+			System.out.println(word+" is a palindrome");
+		}
 	}
 
 }
